650a9bafa70a1c19345f2c8e41355e395f93e12d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-06 19:54:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind
-    valid?
+    
     if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
       raise TriangleError
     elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
